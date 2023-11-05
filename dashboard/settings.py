ff644963f4712a193f5481377ca16e7037f4aef5
lawrencebennet <lorenzobennati.dev@gmail.com>
2023-11-05 22:30:47 +0100

    debug false

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['https://analytics.advepa.eu', 'analytics.advepa.eu', '127.0.0.1', 'localhost']
